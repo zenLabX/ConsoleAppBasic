c105db6bd81e017ade0d62c9b1ecf3bd48bcaadd
zenLabX <simple.forwork2025@gmail.com>
2025-09-19 20:29:41 +0800

    init project

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello World! bbbb");
+        }
+    }
+}
